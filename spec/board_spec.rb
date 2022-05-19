d12dc807129318f0aebe6e3c9bd26937b01ac7e8
Michael Koulouvaris <mikekoul@gmail.com>
2022-05-19 10:24:26 -0600

    Valid coordinate tests

--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -17,4 +17,37 @@ require 'pry'
       expect(board.cells).to be_instance_of(Hash)
       expect(board.cells.length).to eq(16)
     end
+
+    xit 'A1 is a valid coordinate' do
+      board = Board.new
+
+      expect(board.valid_coordinate?("A1")).to be true
+    end
+
+    xit 'D4 is a valid coordinate' do
+      board = Board.new
+
+      expect(board.valid_coordinate?("D4")).to be true
+    end
+
+    xit 'A5 is a valid coordinate' do
+      board = Board.new
+
+      expect(board.valid_coordinate?("A5")).to be false
+    end
+
+    xit 'E1 is a valid coordinate' do
+      board = Board.new
+
+      expect(board.valid_coordinate?("E1")).to be false
+    end
+
+    xit 'A22 is a valid coordinate' do
+      board = Board.new
+
+      expect(board.valid_coordinate?("A22")).to be false
+    end
+
+
+
   end
